ab6b24c5149d2a24de4cdd3599a11a200732354d
Robert Hallowell <rob@snowyplayground.com>
2015-07-24 18:06:42 -0600

    connected cart and product

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,15 @@ app.config(function($routeProvider) {
 		templateUrl : 'scripts/views/user/home/homeTmpl.html',
 		controller : 'homeCtrl'
 	})
+	.when('/product', {
+		templateUrl : 'scripts/views/user/productModal/productModalTmpl.html',
+		controller : 'productModalCtrl'
+	})
+	.when('/cart', {
+		templateUrl : 'scripts/views/user/cart/cartTmpl.html',
+		controller: 'cartCtrl'
+	})
+	
 	
 })
 
---
+++ public/scripts/views/user/checkout/checkoutCtrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('GoldMorning');
+
+app.controller('cartCtrl', function($scope) {
+	$scope.cTest = "This is the cartTmpl.html. D UP from the cartCtrl!"
+});//end productModalCtrl
