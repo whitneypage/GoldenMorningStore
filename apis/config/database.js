55f2e78ea233e9a8bdf5e56f0d0c7b2db3037be8
Andy Johnson <andykj@gmail.com>
2015-07-27 11:13:13 -0600

    fixed routes.js added generic functions to productsCtrl

--- apis/config/database.js
+++ apis/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url': 'mongodb://localhost:27017/appenderApp'
+	'url': 'mongodb://localhost:27017/GoldenMorningStore'
 }
--- apis/controllers/productsCtrl.js
+++ apis/controllers/productsCtrl.js
@@ -2,5 +2,19 @@ var mongoose = require('mongoose');
 var Product = mongoose.model('Product', require('../models/productsSchema.js'));
 
 module.exports = {
+  create: function(req, res){
+  	console.log(req.body);
+  },
 
+  retrieve: function(req, res){
+  	console.log(req.body);
+  },
+
+  update: function(req, res){
+  	console.log(req.body);
+  },
+
+  remove: function(req, res){
+  	console.log(req.body);
+  },
 };
