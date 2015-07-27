9e0ccdaf6e4748b7252b86be60496c95c6b3090e
Tyler Tebbs <tylertebbs20@gmail.com>
2015-07-27 15:28:23 -0600

    'updating'

---
+++ apis/models/productsSchema.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+	sku: {type: Number}
+	, productTitleField: {type: String} 
+	, productDescriptionField: {type: String}
+	, chooseProductCategory: {type: String}
+	, imageField: {type: String}
+	, addPriceField: {type: Number}
+	, dateCreated: {type: Date}
+	, dateUpdated: {type: Date}
+	, colorSize: {
+		color: {type: String}, 
+		sizes: [{
+			smallQty: {type: Number},
+			mediumQty: {type: Number},
+			largeQty: {type: Number}
+		}]
+
+	}
+
+})
+
+productSchema.pre('createProduct', function(){
+		this.dateCreated = Date.now();
+})
