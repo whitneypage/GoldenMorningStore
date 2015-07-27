83c30d6875d2a415ce1c5a42c68a63b06e4ccb3c
Andy Johnson <andykj@gmail.com>
2015-07-27 17:01:13 -0600

    added aws dependency, started s3 integration on server.js added keys to config file

--- server.js
+++ server.js
@@ -9,14 +9,19 @@ var passport = require('passport');
 var session = require('express-session');
 var productsCtrl = require('./apis/controllers/productsCtrl')
 var configDB = require('./apis/config/database.js');
+var AWS = require('aws-sdk');
 var port = 1337;
 
 console.log('configDB.url', configDB.url)
 mongoose.connect('mongodb://localhost:27017/GoldenMorningStore');
 
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname+'/public'));
+
+var s3bucket = new AWS.S3({params: {Bucket: 'goldmorning'}});
+
 
 
 require('./routes.js')(app);
