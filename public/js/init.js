8cf8b238d59a7d1205339ef15acb3758224a0fba
Alicia Chen <toyblox@gmail.com>
2015-07-28 15:56:06 -0600

    get rid of ds store

---
+++ public/js/init.js
@@ -0,0 +1,17 @@
+
+// NAVBAR MAGIC
+
+$(document).ready(function(){
+	$(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 0;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
+
+	$(".button-collapse").sideNav();
+
+});
