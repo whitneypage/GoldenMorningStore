90b8f7be8f5bac78e6f8d9c2596e5b81fa2a03c4
Robert Hallowell <rob@snowyplayground.com>
2015-07-28 17:45:27 -0600

    ng-repeated that shit on the adminHome page -- products return from DB and display on adminHomeTmpl.html

--- public/scripts/services/AdminServices.js
+++ public/scripts/services/AdminServices.js
@@ -2,11 +2,16 @@ var app = angular.module('GoldMorning');
 
 app.service('AdminService', function($http, $q) {
 	this.getAdminProducts = function() {
+		var deferred = $q.defer();
 		$http({
 			method : 'GET',
-			url: ""
-		})
-	}
+			url: "/api/products",
+		}).then(function(response){
+			console.log(response.data, " response.data from getAdminProducts");
+				deferred.resolve(response.data);
+		});//end .then
+			return deferred.promise;
+	};//end AdminService.getAdminProducts
 	
 	
 });// end AdminService
