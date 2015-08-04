9e8795791be2344d1cd896ba30413f2cb55ee5d9
Tyler Tebbs <tylertebbs20@gmail.com>
2015-08-04 10:04:47 -0600

    added colorSizeEd to the order schema

--- apis/models/orderSchema.js
+++ apis/models/orderSchema.js
@@ -4,8 +4,8 @@ var orderSchema = mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
     , products: [{
         product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'}
-        , color: {type: String}
         , size: {type: String}
+        , colorSizeId: {type: String}
     }]
     , total: {type: Number}
     , status: {type: String, enum: ['purchased', 'shipped', 'delivered', 'on hold', 'canceled'], default: 'purchased'}
