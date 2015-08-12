79c648e05ba5d0c23796581e4a0952a16645018f
Andy Johnson <andykj@gmail.com>
2015-08-11 21:02:20 -0600

    created empty cart endpoint and function to destroy the session/empty the cart. tested/working

--- public/scripts/views/user/checkout/thankyouCtrl.js
+++ public/scripts/views/user/checkout/thankyouCtrl.js
@@ -6,6 +6,11 @@ app.controller('thankyouCtrl', function($scope, $routeParams, orderService){
 	console.log($routeParams)
 
 	orderService.updateOrderByPaymentId($routeParams).then(function(data){
+		if(data.data.status === "200"){
+			orderService.emptyCart().then(function(data){
+				console.log('cart is empty now... maybe', data)
+			})
+		}
 		console.log('after order updateOrderByPaymentId', data);
 	})
 
