56f10ed941f6925e0d057c00da4f548ed624015a
Robert Hallowell <rob@snowyplayground.com>
2015-08-12 15:38:37 -0600

    resolved checkoOutCart into thankyouTmpl.html, updated decSize

--- public/scripts/views/user/checkout/thankyouCtrl.js
+++ public/scripts/views/user/checkout/thankyouCtrl.js
@@ -1,18 +1,18 @@
 var app = angular.module('GoldMorning');
 
-app.controller('thankyouCtrl', function($scope, $routeParams, orderService, ProductService, cartService){
+app.controller('thankyouCtrl', function($scope, $routeParams, orderService, ProductService, cartService, checkedOutCart){
+	$scope.checkedOutCart = checkedOutCart;
+	
 	$scope.payerId = $routeParams.PayerID;
 	$scope.name = 'Andy';
 	console.log($routeParams)
 
 	orderService.updateOrderByPaymentId($routeParams).then(function(data){
 		if(data) {
-			cartService.getCart().then(function(checkedOutCart){
-			ProductService.decrementSize(checkedOutCart);
+			
+			ProductService.decrementSize($scope.checkedOutCart);
 			orderService.emptyCart();
-			});	
-
-
+	
 		}
 //		ProductService.decrementSize(data.data.products);
 	});
