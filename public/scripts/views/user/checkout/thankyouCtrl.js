8414c483f2b75561b7db7c09c7fa4f879c53a34b
Robert Hallowell <rob@snowyplayground.com>
2015-08-12 15:02:13 -0600

    added empty cart to thankYouCtrl

--- public/scripts/views/user/checkout/thankyouCtrl.js
+++ public/scripts/views/user/checkout/thankyouCtrl.js
@@ -1,12 +1,17 @@
 var app = angular.module('GoldMorning');
 
-app.controller('thankyouCtrl', function($scope, $routeParams, orderService){
+app.controller('thankyouCtrl', function($scope, $routeParams, orderService, ProductService, cartService){
 	$scope.payerId = $routeParams.PayerID;
 	$scope.name = 'Andy';
 	console.log($routeParams)
 
 	orderService.updateOrderByPaymentId($routeParams).then(function(data){
-		console.log('after order updateOrderByPaymentId', data);
-	})
+		if(data) {
+			cartService.getCart().then(function(checkedOutCart){
+			ProductService.decrementSize(checkedOutCart);	
+			});
+		}
+//		ProductService.decrementSize(data.data.products);
+	});
 
 });
